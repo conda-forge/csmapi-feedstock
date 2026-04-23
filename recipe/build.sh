4c81ca1b2af4fbc07ad8201399b757339059b23e
Adam Paquette <acpaquette@usgs.gov>
2026-04-23 12:25:34 -0600

    Update python variables in cmake setup

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DPython_FIND_VIRTUALENV=FIRST -DPython3_EXECUTABLE=$PYTHON ..
 cmake --build . --config RELEASE
 cmake --install . --config RELEASE
