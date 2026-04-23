2e34ece98ef89fed3188f0d153019c7317572eab
Adam Paquette <acpaquette@usgs.gov>
2026-04-23 13:25:10 -0600

    Add CMAKE_ARGS to cmake command in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE=$PYTHON ..
+cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE=$PYTHON ..
 cmake --build . --config RELEASE
 cmake --install . --config RELEASE
