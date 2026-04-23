dfc8642e7fa123cdebf495ec4e2cbd609a92724f
Adam Paquette <acpaquette@usgs.gov>
2026-04-23 12:16:26 -0600

    Update build commands

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DANACONDA_PYTHON_VERBOSE=ON ..
-cmake --build .
-cd python
-$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
-cd ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake --build . --config RELEASE
+cmake --install . --config RELEASE
